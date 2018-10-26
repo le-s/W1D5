1c4c856e033584791817a640a75bd4f92d5eddfa
le-s <steven.an.le@gmail.com>
2018-10-26 13:57:17 -0700

    Redefine #parent= to be able to change parent & delete parent without issue

--- Intro Algorithms/skeleton 2/lib/00_tree_node.rb
+++ Intro Algorithms/skeleton 2/lib/00_tree_node.rb
@@ -1,4 +1,5 @@
 class PolyTreeNode
+  attr_accessor :parent, :children, :value
 
   def initialize(value)
     @parent = nil
@@ -11,7 +12,20 @@ class PolyTreeNode
   end
 
   def parent=(node)
-    @parent = node
+    if @parent == nil
+      @parent = node
+      node.children << self
+    elsif node == nil
+      @parent.children.delete(self)
+      @parent = nil
+    else
+      unless @parent == node
+        temp_parent = @parent
+        temp_parent.children.delete(self)
+        @parent = node
+        node.children << self
+      end
+    end
   end
 
   def children
